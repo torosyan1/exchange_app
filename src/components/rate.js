141048aab9810e8c3a02421c19955e14897a263b
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2024-07-19 14:48:02 +0400

    fix

--- src/components/rate.js
+++ src/components/rate.js
@@ -3,26 +3,58 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Divider } from '@mui/material';
 
 function RateChanger() {
-  const [rate, setRate] = useState(0);
+  const [rateSell, setRateSell] = useState(0);
+  const [rateBuy, setRateBuy] = useState(0);
 
-  const handleRateChange = (event) => {
-    setRate(event.target.value);
+  const handleRateChangeBuy = (event) => {
+    setRateBuy(event.target.value);
   };
 
-  const handleRateSubmit = async () => {
+
+  const handleRateChangeSell = (event) => {
+    setRateSell(event.target.value);
+  };
+
+
+  const handleRateSubmitBuy = async () => {
     try {
-      const response = await fetch('http://localhost:6990/api/update-rate', {
+      const response = await fetch('http://51.20.225.234:6990/api/update-rate-buy', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token')
         },
-        body: JSON.stringify({ rate }),
+        body: JSON.stringify({ rate: rateBuy }),
+      });
+      if (response.ok) {
+        alert('Buy rate updated')
+        console.log('Rate updated successfully');
+        // Optionally update local state or show a success message
+      } else {
+        console.error('Failed to update rate');
+        // Handle error scenario
+      }
+    } catch (error) {
+      console.error('Error updating rate:', error);
+      // Handle network error scenario
+    }
+  };
+
+  const handleRateSubmitSell = async () => {
+    try {
+      const response = await fetch('http://51.20.225.234:6990/api/update-rate-sell', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token')
+        },
+        body: JSON.stringify({ rate: rateSell }),
       });
       if (response.ok) {
         console.log('Rate updated successfully');
+        alert('Sell rate updated')
         // Optionally update local state or show a success message
       } else {
         console.error('Failed to update rate');
@@ -37,10 +69,19 @@ function RateChanger() {
   useEffect(() => {
     const fetchLastRate = async () => {
       try {
-        const response = await fetch('http://51.20.225.234:6990/api/last-rate', { headers: { Authorization: localStorage.getItem('token') }});
-        if (response.ok) {
-          const lastRate = await response.json();
-          setRate(lastRate.value || 0);
+        const responseSell = await fetch('http://51.20.225.234:6990/api/last-rate-sell', { headers: { Authorization: localStorage.getItem('token') }});
+        if (responseSell.ok) {
+          const lastRate = await responseSell.json();
+          setRateSell(lastRate.value || 0);
+        } else {
+          console.error('Failed to fetch last rate');
+          // Handle error scenario
+        }
+
+        const responseBuy = await fetch('http://51.20.225.234:6990/api/last-rate-buy', { headers: { Authorization: localStorage.getItem('token') }});
+        if (responseBuy.ok) {
+          const lastRate = await responseBuy.json();
+          setRateBuy(lastRate.value || 0);
         } else {
           console.error('Failed to fetch last rate');
           // Handle error scenario
@@ -55,38 +96,39 @@ function RateChanger() {
   }, []);
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
+    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mt: 2 }}>
+     <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%'  }}>
+     <div>
       <Typography variant="h6" sx={{ mt: 2 }}>
-        Sell Rate: {rate}
+        Buy Rate: {rateBuy}
       </Typography>
-      <Divider/>
       <TextField
         label="Enter Rate"
         type="number"
-        value={rate}
-        onChange={handleRateChange}
+        value={rateBuy}
+        onChange={handleRateChangeBuy}
         variant="outlined"
       />
-      <Button variant="contained" onClick={handleRateSubmit} sx={{ mt: 2 }}>
+      <Button style={{ display: 'block', width: '100%' }} variant="contained" onClick={handleRateSubmitBuy} sx={{ mt: 2 }}>
         Update Rate
       </Button>
-
-      <Divider/>
-      
+      </div>
+      <div>
       <Typography variant="h6" sx={{ mt: 2 }}>
-        Buy Rate: {rate}
+        Sell Rate: {rateSell}
       </Typography>
-      <Divider/>
       <TextField
         label="Enter Rate"
         type="number"
-        value={rate}
-        onChange={handleRateChange}
+        value={rateSell}
+        onChange={handleRateChangeSell}
         variant="outlined"
       />
-      <Button variant="contained" onClick={handleRateSubmit} sx={{ mt: 2 }}>
+      <Button style={{ display: 'block', width: '100%' }} variant="contained" onClick={handleRateSubmitSell} sx={{ mt: 2 }}>
         Update Rate
       </Button>
+        </div>  
+        </div>    
     </Box>
   );
 }
